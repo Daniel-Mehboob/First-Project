38230029bfef22895b0ed7defc0b29c02aa98121
Danial Mehboob <danialmehmboob213@gmail.com>
2024-11-20 13:07:27 +0500

    AuthToken plus Hashing in the backend

--- backend/models/User.js
+++ backend/models/User.js
@@ -21,5 +21,5 @@ const UserSchema = new Schema({
   },
 });
 
-const UserModel = mongoose.model("user", UserSchema);
-module.exports = UserModel;
+const User = mongoose.model("user", UserSchema);
+module.exports = User;
--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,21 +1,64 @@
 const express = require("express");
 const User = require("../models/User");
 const router = express.Router();
-const { query, validationResult } = require("express-validator");
+const { body, validationResult } = require("express-validator");
+const bcrypt = require("bcryptjs");
+var jwt = require("jsonwebtoken");
+
+const JWT_SECRECT = "DaniAlvesismynic$name";
+
+// Create a User using POST "/api/aut/createuser". No login required
 
 router.post(
-  "/",
+  "/createuser",
   [
-    body("name").isLength({ min: 3 }),
-    body("email").isEmail(),
-    body("password").isLength({ min: 5 }),
+    body("name", "Enter a Valid name").isLength({ min: 3 }),
+    body("email", "Enter a valid email").isEmail(),
+    body("password", "Password must be of at least 5 characters").isLength({
+      min: 5,
+    }),
   ],
-  (req, res) => {
+  async (req, res) => {
+    // if there are errors return the bad requests and the errors
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() });
     }
-    res.send(req.body);
+    console.log(req.body);
+
+    // check wether the user with this email exists already
+    try {
+      let user = await User.findOne({ email: req.body.email });
+      if (user) {
+        return res
+          .status(400)
+          .json({ error: "Sorry a user with this email already exists" });
+      }
+
+      const salt = await bcrypt.genSalt(10);
+      const secPass = await bcrypt.hash(req.body.password, salt);
+
+      // Create a new user
+      user = await User.create({
+        name: req.body.name,
+        password: secPass,
+        email: req.body.email,
+      });
+
+      const data = {
+        id: user.id,
+      };
+
+      const jwtData = jwt.sign(data, JWT_SECRECT);
+      console.log(jwtData);
+
+      res.json({ authtoken: jwtData });
+
+      // Catch Errors
+    } catch (error) {
+      console.error(error.message);
+      res.status(500).send("some error occured");
+    }
   }
 );
 
